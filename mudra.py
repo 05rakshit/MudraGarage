8ef8ee9aa474a4c88879e35b87bf4b638db6b0ee
05rakshit <05rakshitgarg@gmail.com>
2025-04-10 18:58:46 +0530

    Adding new files/Modifying existing files

--- mudra.py
+++ mudra.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route("/Tools")
 def tools_page():
