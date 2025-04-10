7725f1e8f1026fec88ade778740b114a74dbd57d
05rakshit <05rakshitgarg@gmail.com>
2025-04-10 18:06:37 +0530

    Changes

--- mudra.py
+++ mudra.py
@@ -6,3 +6,20 @@ app=Flask(__name__)
 @app.route("/home")
 def home_page():
     return render_template('index.html')
+
+@app.route("/Tools")
+def tools_page():
+    pass
+
+
+@app.route("/Products")
+def products_page():
+    pass
+
+@app.route("/login")
+def login_page():
+    pass
+
+@app.route("/register")
+def register_page():
+    pass
